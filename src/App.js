2f72d56245ef4fcf85579e4f904da4c498b00a03
Alademehin Samuel <alademahinsamuel@yahoo.com>
2023-07-20 01:07:03 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    return (
+        <div className='App'>
+    <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
+        <Container>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                Separated link
+                </NavDropdown.Item>
+            </NavDropdown>
+            </Nav>
+            <Nav>
+            <Nav.Link href="#deets">More deets</Nav.Link>
+            <Nav.Link eventKey={2} href="#memes">
+                Dank memes
+            </Nav.Link>
+            </Nav>
+        </Navbar.Collapse>
+        </Container>
+    </Navbar>
+    <div className='App'>
+        <header className='App-header'>
+    <Row xs={1} md={2} className="g-4">
+        {Array.from({ length: 4 }).map((_, idx) => (
+        <Col key={idx}>
+            <Card className='mb-3' style={{color: '#000'}}>
+            <Card.Img src='https://picsum.photos/200/100' />
+            <Card.Body>
+                <Card.Title>Card title</Card.Title>
+                <Card.Text>
+                This is a longer card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+                </Card.Text>
+            </Card.Body>
+            </Card>
+        </Col>
+        ))}
+    </Row>
+    </header>
     </div>
-  );
+    </div>
+);
 }
 
 export default App;
+
+
